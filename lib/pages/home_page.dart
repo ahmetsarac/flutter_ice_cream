f182bff9b9738ce13acb0eb72fb8c39890bae1c0
Ahmet Saraç <ahmet.src.99@gmail.com>
2022-02-10 02:46:57 +0300

    Top section is added.

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/top_section.dart';
+
+class HomePage extends StatelessWidget{
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(
+      child: Container(
+          padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TopSection(),
+          ],
+            ),
+      ),
+    ); 
+  }
+}
---
+++ lib/widgets/top_section.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TopSection extends StatelessWidget {
+  const TopSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('Hey Emma', style: TextStyle(fontSize: 30)),
+            Text('What flavor do you like to eat?', style: TextStyle(color: Colors.grey[600])),
+          ],
+        ),
+        CircleAvatar(radius: 28, backgroundColor: Theme.of(context).primaryColor),
+      ],
+    );
+  }
+}
